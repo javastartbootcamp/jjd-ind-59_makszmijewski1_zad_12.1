df1f9e1a9c98c27a1e2929fdaf945a550da45164
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-25 13:41:58 +0100

    pierwszy commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,50 @@
+import java.io.*;
+
 class Main {
     public static void main(String[] args) {
-        // uzupełnij rozwiązanie
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("numbers.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                calculateAndPrint(line);
+                saveToResults(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void saveToResults(String line) {
+        String[] numbersAndOperator = line.split(" ");
+        double a = Double.parseDouble(numbersAndOperator[0]);
+        double b = Double.parseDouble(numbersAndOperator[2]);
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("results.txt", true));
+            switch (numbersAndOperator[1]) {
+                case "+" -> writer.write(line + " = " + (a + b) + "\n");
+                case "-" -> writer.write(line + " = " + (a - b) + "\n");
+                case "*" -> writer.write(line + " = " + (a * b) + "\n");
+                case "/" -> writer.write(line + " = " + (a / b) + "\n");
+                default -> writer.write("nieprawidłowe dane" + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void calculateAndPrint(String line) {
+        String[] numbersAndOperator = line.split(" ");
+        double a = Double.parseDouble(numbersAndOperator[0]);
+        double b = Double.parseDouble(numbersAndOperator[2]);
+
+        switch (numbersAndOperator[1]) {
+            case "+" -> System.out.println(line + " = " + (a + b));
+            case "-" -> System.out.println(line + " = " + (a - b));
+            case "*" -> System.out.println(line + " = " + (a * b));
+            case "/" -> System.out.println(line + " = " + (a / b));
+            default -> System.out.println("nieprawidłowe dane");
+        }
     }
 }
